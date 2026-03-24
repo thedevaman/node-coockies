4bad4362d5a1ac38de103c6e042ce6be221a9f27
thedevaman <devaman2508@gmail.com>
2026-03-24 09:07:16 +0530

    coockie data fetched

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express();
 
 app.set("view engine",'ejs')
+app.use(express.urlencoded({extended:true})) //used to make requested body in readable form
 
 //app.set('views', './templates') //custome folder
 
@@ -12,10 +13,15 @@ res.render('login')
 
 
 app.post('/profile',(req,res)=>{
+    res.setHeader('Set-Cookie',"login=true")
+    res.setHeader('Set-Cookie',"name="+req.body.name)
 res.render('profile')
 })
 
 app.get('/',(req,res)=>{
-res.render('home')
+    let cookies_data = req.get("cookie");
+    let cookiesData = cookies_data.split(";");
+    let data = cookiesData[1].split("=")
+res.render('home',{name:data[1]})
 })
 app.listen(3200)
